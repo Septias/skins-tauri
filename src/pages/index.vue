a273bf3a61603fc41a0ec09aae949d1d81c50690
Septias <scoreplayer2000@gmail.com>
2023-04-25 12:18:08 +0200

    change display

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,21 +5,25 @@ import type { MarketPrice } from 'src-tauri/bindings/MarketPrice'
 
 const user_input = useStorage('user_id', '')
 const error = ref('')
-const chests: Ref<FullAsset[]> = ref([])
-const prices: Ref<{ [index: number]: MarketPrice }> = ref({})
+const chests: Ref<FullAsset[]> = useStorage('chests', [])
+const prices: Ref<{ [index: number]: MarketPrice }> = useStorage('prices', {})
 
 function update() {
   invoke('get_user_containers', { game: 730, user: user_input.value }).then((res) => {
     chests.value = res as FullAsset[]
     console.log(res)
+  }).catch((err) => {
+    error.value = err
   })
   invoke('get_asset_prices', { assets: chests.value.map(chest => chest.classid) }).then((res) => {
     console.log(res)
     prices.value = res as { [index: number]: MarketPrice }
+  }).catch((err) => {
+    error.value = err
   })
 }
 
-const total_value = chests.value.map(chest => chest.amount * prices.value[chest.classid].median_price).reduce((a, b) => a + b, 0).toFixed(2)
+// const total_value = chests.value.map(chest => chest.amount * (prices.value[chest.classid].median_price || 0)).reduce((a, b) => a + b, 0).toFixed(2)
 </script>
 
 <template lang="pug">
@@ -30,11 +34,12 @@ div.c-grid.p-10
       div.flex.gap-2
         input.rounded.p-2.border.leading-none(v-model="user_input" alt="User ID" placeholder="User ID")
         button.rounded.bg-rose-500.p-2.leading-none(@click="update") Update
-      div Total chest value: {{ total_value }}€
+      //div Total chest value: {{ total_value }}€
     div.chest-grid
       div.border.border-rose-500.rounded-xl.p-2.shadow-xl(v-for="chest in chests")
-        h1.text-xl.font-bold {{ chest.name }}
-        p {{ chest.amount }}
+        h1.text-xl.font-bold {{ chest.amount }} x {{ chest.name }}
+        div.flex.justify-center
+          img(:src="'https://community.akamai.steamstatic.com/economy/image/' + chest.icon_url")
         p(v-if="prices.value[chest.classid]") {{ (chest.amount * chest.price.median_price).toFixed(2) }}€
 </template>
 
